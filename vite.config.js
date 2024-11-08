d587234322409d2a3caa32886604344903a67752
Omar Elewa <omarelewa25@gmail.com>
2024-11-08 18:05:27 +0200

    lower resources

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    build: {
+        sourcemap: false, // Disable source maps to save memory
+        target: 'es2015', // Set a lower build target if compatible
+        minify: 'esbuild',
+        esbuild: {
+            concurrency: 1 // Reduce the number of concurrent threads
+        }
+    },
     plugins: [
         laravel({
             input: [
